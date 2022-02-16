[fix] disp_map calculation in render_rays().

run_nerf.py
@@ -143,18 +143,18 @@ def render_rays(ray_batch,
             tf.math.cumprod(1.-alpha + 1e-10, axis=-1, exclusive=True)
 
         # Computed weighted color of each sample along each ray.
-        rgb_map = tf.reduce_sum(
+        rgb_map = tf.math.reduce_sum(
             input_tensor=weights[..., None] * rgb, axis=-2)  # [N_rays, 3]
 
         # Estimated depth map is expected distance.
-        depth_map = tf.reduce_sum(input_tensor=weights * z_vals, axis=-1)
+        depth_map = tf.math.reduce_sum(input_tensor=weights * z_vals, axis=-1)
 
         # Disparity map is inverse depth.
-        disp_map = 1./tf.maximum(1e-10, depth_map /
-                                 tf.reduce_sum(input_tensor=weights, axis=-1))
+        disp_map = 1./tf.math.maximum(1e-10, depth_map /
+                                 tf.math.reduce_sum(input_tensor=weights, axis=None))
 
         # Sum of weights along each ray. This value is in [0, 1] up to numerical error.
-        acc_map = tf.reduce_sum(input_tensor=weights, axis=-1)
+        acc_map = tf.math.reduce_sum(input_tensor=weights, axis=-1)
 
         # To composite onto a white background, use the accumulated alpha map.
         if white_bkgd:

run_nerf_helpers.py
@@ -125,7 +125,7 @@ def get_rays(H, W, focal, c2w):
     i, j = tf.meshgrid(tf.range(W, dtype=tf.float32),
                        tf.range(H, dtype=tf.float32), indexing='xy')
     dirs = tf.stack([(i-W*.5)/focal, -(j-H*.5)/focal, -tf.ones_like(i)], -1)
-    rays_d = tf.reduce_sum(input_tensor=dirs[..., np.newaxis, :] * c2w[:3, :3], axis=-1)
+    rays_d = tf.math.reduce_sum(input_tensor=dirs[..., np.newaxis, :] * c2w[:3, :3], axis=-1)
     rays_o = tf.broadcast_to(c2w[:3, -1], tf.shape(input=rays_d))
     return rays_o, rays_d
 
@@ -184,7 +184,7 @@ def sample_pdf(bins, weights, N_samples, det=False):
 
     # Get pdf
     weights += 1e-5  # prevent nans
-    pdf = weights / tf.reduce_sum(input_tensor=weights, axis=-1, keepdims=True)
+    pdf = weights / tf.math.reduce_sum(input_tensor=weights, axis=-1, keepdims=True)
     cdf = tf.cumsum(pdf, -1)
     cdf = tf.concat([tf.zeros_like(cdf[..., :1]), cdf], -1)
 